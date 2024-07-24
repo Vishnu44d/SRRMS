71544afbd3ceb1174361b62602a677b9f40959a9
unknown <anandayush81@gmail.com>
2024-07-24 22:02:19 +0530

    first

--- environment/SRRMSv2/api/status.py
+++ environment/SRRMSv2/api/status.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 statusBP = Blueprint('StatusAPI', __name__)
-
+#Added a comment -Ayush
 @statusBP.route('/', methods=['GET', 'POST'])
 def get_status():
     return 'abc'
